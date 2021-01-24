fa1ca9f7078094b9c5b46015c4b56317d757443c
Dylan Parson <dylan.parson001@gmail.com>
2021-01-24 10:02:57 -0500

    added DOM methods, not finished yet

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,41 +1,81 @@
-rockPaperScissors();
+//event listener for clicking rock button
+const rock = document.querySelector("#rock"); // selects rock id
+    rock.addEventListener("click", () => { // event listener 
+    let choice = "rock";
+    rockPaperScissors(choice);
+})
 
-// Full rock paper scissors game
-function rockPaperScissors(){
+const paper = document.querySelector("#paper");
+    paper.addEventListener("click", () => {
+    let choice = "paper";
+    rockPaperScissors(choice);
+})
+
+const scissors = document.querySelector("#scissors");
+    scissors.addEventListener("click", () => {
+    let choice = "scissors";
+    rockPaperScissors(choice);
+})
+
+
+let tie = () => {
+    const results = document.querySelector(".results"); 
+    const tie = document.createElement('h3');
+    tie.textContent = "It is a tie!";
+    results.appendChild(tie);
+
+}
+
+let playerWin = () =>{
+    const results = document.querySelector(".results"); 
+    const player = document.createElement('h3');
+    player.textContent = "You win!";
+    results.appendChild(player)
+}
+
+let computerWin = () => {
+    const results = document.querySelector(".results"); 
+    const computer = document.createElement('h3');
+    computer.textContent = "Computer wins!";
+    results.appendChild(computer)
+}
+
+
+// Full rock paper scissors game function
+function rockPaperScissors(choice){
     const rock = 1;
     const paper = 2;
     const scissors = 3;
-    let playerChoice ;
     let computer;
     let winner;
     let playerWins = 0;
     let computerWins = 0;
 
-    for (let i = 0; i < 5; i++){ // plays the game 5 times
-        playerChoice = gameChoice(); // gets user input
-        computer = computerChoice(); // gets random number 1-3 and converts it to a string
-        winner = gameDecision(playerChoice, computer); // decides winner
+    const score = document.querySelector(".score");
+    /*const playerScore = score.createElement('h1');
+    const computerScore = score.createElement("h1");
+    playerScore.textContent = "Player score: " + playerWins;
+    computerScore.textContent = "Computer score: " + computerWins;
+    score.appendChild(playerScore);
+    score.appendChild(computerScore);*/
 
-        if(winner === 1){
-            playerWins++; // keeps track of amount of wins for the user
-        }
-        else if (winner === 2)
-            computerWins++; // keeps track of amount of wins for the computer
-        }
-        console.log("Player has won " + playerWins + " times");
-        console.log("Computer has won " + computerWins + " times");
+    computer = computerChoice(); // gets random number 1-3 and converts it to a string
+    winner = gameDecision(choice, computer); // decides winner
 
-        if (computerWins > playerWins){ // if the computer had more wins total
-            console.log("Computer wins!");
-        }
-        else if (playerWins > computerWins){ // if the player had more wins total
-            console.log("You win!");
-        }
+    if(winner === 1){
+        playerWins++; // keeps track of amount of wins for the user
+    }
+    else if (winner === 2){
+        computerWins++; // keeps track of amount of wins for the computer
+    }
+    
+    
         
 }   
 //Gets random choice for computer
 function computerChoice(){
     let choice = Math.floor(Math.random() * 3) + 1;
+    
     if (choice === 1){
         choice = "rock";
     }
@@ -46,44 +86,44 @@ function computerChoice(){
         choice = "paper";
     }
     console.log("The computer chose " + choice);
-    return choice;
-}
+    const comChoice = document.querySelector(".results");
+    const displayResult = document.createElement('h3');
+    displayResult.textContent = "The computer chose " + choice;
+    comChoice.appendChild(displayResult);
 
-// gets input from user
-function gameChoice(){
-    let choice = prompt("Choose Rock Paper or Scissors");
-    console.log("You chose " + choice);
     return choice;
 }
 
 //decides whether computer or player wins
+
 function gameDecision(playerChoice, computerChoice){
+   
     if (playerChoice === computerChoice){
-        console.log("It is a tie!");
+        tie();
         return 0;
     }
-    else if (playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors"){
-        console.log("Player wins!");
+    if (playerChoice === "rock" && computerChoice === "scissors"){
+        playerWin();
         return 1;
     }
-    else if (playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper"){
-        console.log("Computer wins!");
+    if (playerChoice === "rock" && computerChoice === "paper"){
+        computerWin();
         return 2;
     }
-    else if (playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "paper"){
-        console.log("Player wins!");
+    if (playerChoice === "scissors" && computerChoice === "paper"){
+        playerWin();
         return 1;
     }
-    else if (playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock"){
-        console.log("Computer wins!");
+    if (playerChoice === "scissors" && computerChoice === "rock"){
+        computerWin();
         return 2;
     }
-    else if (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors"){
-        console.log("Computer wins!");
+    if (playerChoice === "paper" && computerChoice  === "scissors"){
+        computerWin();
         return 2;
     }
-    else if (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock"){
-        console.log("Player wins!");
+    if (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock"){
+        playerWin();
         return 1;
     }
     
